79ec5258f0ebfd46f0860ceb0aa790cff3a22643
Parth Krishna <parthkrishna1611@gmail.com>
2019-01-27 00:39:00 +0530

    uses PorterStemmer to stem the words

--- inaugural.py
+++ inaugural.py
@@ -1,6 +1,7 @@
 import nltk
 from nltk.corpus import inaugural, stopwords
 from nltk import sent_tokenize, word_tokenize
+from nltk.stem import *
 import pprint
 
 # initialize pprint
@@ -13,6 +14,7 @@ data = '1789-Washington.txt'
 
 # get speech of particular file
 speech = inaugural.raw(data)
+speech = speech.lower()
 
 # get sentences
 sentences = inaugural.sents(data)
@@ -32,11 +34,27 @@ word_tokens = word_tokenize(speech)
 stop_words = set(stopwords.words('english'))
 #pp.pprint(stop_words)
 
-
+# remove stop words from speech
 filtered_sentence = [w for w in word_tokens if not w in stop_words]
 
+'''
 for w in word_tokens:
     if w not in stop_words:
         filtered_sentence.append(w)
+'''
 
 pp.pprint(filtered_sentence)
+
+
+# initialize stemmer
+stemmer = PorterStemmer()
+
+stemmed_words = []
+for word in filtered_sentence:
+    stemmed = stemmer.stem(word)
+    stemmed_words.append(stemmed)
+
+pp.pprint(stemmed_words)
+
+
+
